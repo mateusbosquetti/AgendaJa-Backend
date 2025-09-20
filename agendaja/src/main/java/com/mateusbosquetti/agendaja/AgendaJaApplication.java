bd542af9e5dbd8206b4fcf17b53b35635aac2e44
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-09-20 11:40:38 -0300

    feat: criação do projeto Spring

---
+++ agendaja/src/main/java/com/mateusbosquetti/agendaja/AgendaJaApplication.java
@@ -0,0 +1,13 @@
+package com.mateusbosquetti.agendaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaJaApplication.class, args);
+	}
+
+}
---
+++ agendaja/src/test/java/com/mateusbosquetti/agendaja/AgendaJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusbosquetti.agendaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
