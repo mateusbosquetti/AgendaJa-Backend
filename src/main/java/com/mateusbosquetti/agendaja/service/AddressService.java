ee5d920147f577201d20c555e585f6f9c0ca296c
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-10-04 22:19:48 -0300

    feat: implement createAddress method in AddressService

--- src/main/java/com/mateusbosquetti/agendaja/service/AddressService.java
+++ src/main/java/com/mateusbosquetti/agendaja/service/AddressService.java
@@ -1,9 +1,25 @@
 package com.mateusbosquetti.agendaja.service;
 
+import com.mateusbosquetti.agendaja.mapper.AddressMapper;
+import com.mateusbosquetti.agendaja.model.dto.request.AddressRequestDTO;
+import com.mateusbosquetti.agendaja.model.entity.Address;
+import com.mateusbosquetti.agendaja.repository.AddressRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class AddressService {
+
+    private final AddressRepository repository;
+
+    public ResponseEntity<Address> createAddress(AddressRequestDTO requestDTO) {
+        Address savedAddress = AddressMapper.toEntity(requestDTO);
+        savedAddress = repository.save(savedAddress);
+        return ResponseEntity.ok(savedAddress);
+    }
+
 }
